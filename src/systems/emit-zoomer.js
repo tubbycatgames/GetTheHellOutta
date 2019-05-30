e75d62763337d11e54cc96401d86cac0c1e5af24
Phil Simmons <phil@tubbycatgames.com>
2019-05-30 08:55:10 -0400

    Change Zoomer to emit from every direction and move config to constants

--- src/systems/emit-zoomer.js
+++ src/systems/emit-zoomer.js
@@ -1,8 +1,54 @@
 import { Vector } from "../components";
 import { Zoomer } from "../renderers";
 
+export const color = "#ff4040";
 export const emissionGapMs = 500;
 export const maxZoomers = 20;
+export const velocity = 100;
+
+const generateZoomerMovement = ({ height, width }) => {
+  let posX, posY, velX, velY;
+
+  const side = Math.random() * 4;
+  if (side > 3) {
+    // Left
+    posX = -10;
+    posY = Math.random() * height;
+    velX = velocity;
+    velY = 0;
+  } else if (side > 2) {
+    // Bottom
+    posX = Math.random() * width;
+    posY = height + 10;
+    velX = 0;
+    velY = -velocity;
+  } else if (side > 1) {
+    // Right
+    posX = width + 10;
+    posY = Math.random() * height;
+    velX = -velocity;
+    velY = 0;
+  } else {
+    // Top
+    posX = Math.random() * width;
+    posY = -10;
+    velX = 0;
+    velY = velocity;
+  }
+
+  return [new Vector(posX, posY), new Vector(velX, velY)];
+};
+
+const createZoomer = (screen, createdAt) => {
+  const [position, velocity] = generateZoomerMovement(screen);
+  return {
+    color,
+    createdAt,
+    position,
+    renderer: Zoomer,
+    velocity
+  };
+};
 
 const emitZoomer = (entities, { screen, time }) => {
   const zoomers = Object.values(entities).filter(
@@ -16,16 +62,9 @@ const emitZoomer = (entities, { screen, time }) => {
       .reduce((acc, value) => Math.max(acc, value), 0);
     const isZoomerDue = lastZoomerCreationTime + emissionGapMs < time.current;
     if (isZoomerDue) {
-      const x = Math.random() * screen.width;
       return {
         ...entities,
-        ["zoomer" + time.current]: {
-          color: "#ff4040",
-          createdAt: time.current,
-          position: new Vector(x, -20),
-          renderer: Zoomer,
-          velocity: new Vector(0, 100)
-        }
+        ["zoomer" + time.current]: createZoomer(screen, time.current)
       };
     }
   }
